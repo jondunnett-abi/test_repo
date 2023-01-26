d937d1dcaace6855da2e29600cdc1dd0b346e9cc
Jon Dunnett <98055293+jondunnett-abi@users.noreply.github.com>
2023-01-26 15:10:58 -0500

    something.sql

---
+++ something.sql
@@ -0,0 +1,11 @@
+select * from xyz inner join yxz on yxz.join_key = xyz.other_join_key and xyz.filter_key = 'hello';
+
+select 
+  1,
+     2, 
+      
+      
+        3,
+    4
+from 
+  something
